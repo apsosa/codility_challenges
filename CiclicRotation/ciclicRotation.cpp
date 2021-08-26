1d2423dc87ca1f431916d0da4793b277ef6d57d9
pato091 <patriciososa91@gmail.com>
2021-08-26 16:15:17 -0300

    ciclic rotation finish

---
+++ CiclicRotation/ciclicRotation.cpp
@@ -0,0 +1,41 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+void rotation(vector<int> &A){
+
+
+}
+
+
+vector<int> solution(vector<int> &A, int K){
+    int lon = A.size();
+    if(lon == 0){
+        vector<int> empty = {};
+        return empty;
+    }
+    int shif= K % lon;
+    int index = 0;
+    vector<int> tmp(A.size(),0);
+    while(index < lon){
+        tmp[shif] = A[index];
+        shif = (shif+1) % lon;
+        index++;
+    }
+    return tmp;
+}
+
+int main(int argc, char const *argv[])
+{
+
+    // vector<int> A2 = {3, 8, 9, 7, 6};
+    // vector<int> A2 = {1, 2, 3, 4};
+    vector<int> A2 = {};
+    A2 = solution(A2,4);
+    for (size_t i = 0; i < A2.size(); i++)
+    {
+        cout<< A2[i];
+    }
+    
+    return 0;
+}
